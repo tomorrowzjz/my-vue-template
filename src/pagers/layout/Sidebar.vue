3a4c4fd9e5ff5beb85ba76d1a51a91e8bd2afc26
tomorrowzjz <1047547064@qq.com>
2019-07-10 17:42:41 +0800

    '改变router动态渲染sidebar'

--- src/pagers/layout/Sidebar.vue
+++ src/pagers/layout/Sidebar.vue
@@ -4,102 +4,103 @@
              background-color="#545c64"
              unique-opened
              text-color="#fff" active-text-color="#ffd04b" style="border: none">
-      <el-submenu index="/1">
-        <template slot="title"><i class="el-icon-message navigation"></i><span slot="title">VUE</span>
-        </template>
-        <el-menu-item-group>
-          <el-menu-item index="/testv-model">testv-model</el-menu-item>
-          <el-menu-item index="/testcascader">testcascader</el-menu-item>
-          <el-menu-item index="/mixins">mixins</el-menu-item>
-          <el-menu-item index="/sync">sync</el-menu-item>
-          <el-menu-item index="/transitions">transitions</el-menu-item>
-          <el-menu-item index="/directive">directive</el-menu-item>
-          <el-menu-item index="/provideInject">provideInject</el-menu-item>
-          <el-menu-item index="/attrListener">attrListener</el-menu-item>
-          <el-menu-item index="/NativeDrag">NativeDrag</el-menu-item>
-          <el-menu-item index="/extendTable">extendTable</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
-      <el-submenu index="/2">
-        <template slot="title"><i class="el-icon-eleme navigation"></i><span slot="title">ELEMENTUI</span>
-        </template>
-        <el-menu-item-group>
-          <el-menu-item index="/table">table</el-menu-item>
-          <el-menu-item index="/form">form</el-menu-item>
-          <el-menu-item index="/transfer">transfer</el-menu-item>
-          <el-menu-item index="/checkboxDemo">checkboxDemo</el-menu-item>
-          <el-menu-item index="/validtorForm">validtorForm</el-menu-item>
-          <el-menu-item index="/mock">mock</el-menu-item>
-          <el-menu-item index="/caneditortable">caneditortable</el-menu-item>
-          <el-menu-item index="/eleeditortable">eleeditortable</el-menu-item>
-          <el-menu-item index="/fatherdrag">fatherdrag</el-menu-item>
-          <el-menu-item index="/mergeTable">mergeTable</el-menu-item>
-          <el-menu-item index="/betterTables">betterTables</el-menu-item>
-          <el-menu-item index="/two-lists">two-lists</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
-      <el-submenu index="/3">
-        <template slot="title"><i class="el-icon-menu navigation"></i><span slot="title">第三方类库</span>
-        </template>
-        <el-menu-item-group>
-          <el-menu-item index="/SKU">SKU</el-menu-item>
-          <el-menu-item index="/handsontable">handsontable</el-menu-item>
-          <el-menu-item index="/image-lazy">image-lazy</el-menu-item>
-          <el-menu-item index="/html2canvas">html2canvas</el-menu-item>
-          <el-menu-item index="/enlarge">enlarge</el-menu-item>
-          <el-menu-item index="/better-scroll">better-scroll</el-menu-item>
-          <el-menu-item index="/echarts">echarts</el-menu-item>
-          <el-menu-item index="/waterfall">waterfall</el-menu-item>
-          <el-menu-item index="/qrcode">二维码</el-menu-item>
-          <el-menu-item index="/imageEditor">imageEditor</el-menu-item>
-          <el-menu-item index="/imagecropper">imagecropper</el-menu-item>
-          <el-menu-item index="/vue-scrollto">vue-scrollto</el-menu-item>
-          <el-menu-item index="/vue-fabric">vue-fabric</el-menu-item>
-          <el-menu-item index="/mergeImages">mergeImages</el-menu-item>
-          <el-menu-item index="/baidu-map">baidu-map</el-menu-item>
-          <el-menu-item index="/animate">animate</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
-      <el-submenu index="/4">
-        <template slot="title"><i class="el-icon-document navigation"></i><span slot="title">NetWork</span>
-        </template>
-        <el-menu-item-group>
-          <el-menu-item index="/loading">loading</el-menu-item>
-          <el-menu-item index="/scrollTo">scrollTo</el-menu-item>
-          <el-menu-item index="/xmForm">xmForm(N)</el-menu-item>
-          <el-menu-item index="/TESTS">vuedraggable</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
-      <el-submenu index="/5">
-        <template slot="title"><i class="el-icon-postcard navigation"></i><span slot="title">Example</span>
-        </template>
-        <el-menu-item-group>
-          <el-menu-item index="/menu">menu</el-menu-item>
-          <!--<el-menu-item index="/vueScrollTo">menu</el-menu-item>-->
-          <el-submenu index="/">
-            <template slot="title">兄弟组件传值</template>
-            <el-menu-item index="/brotherone">brotherone</el-menu-item>
-            <el-menu-item index="/brothertwo">brothertwo</el-menu-item>
-          </el-submenu>
-          <el-submenu index="/zjz">
-            <template slot="title">jsx-render</template>
-            <el-menu-item index="/render">render</el-menu-item>
-            <el-menu-item index="/renderindex">renderindex</el-menu-item>
-          </el-submenu>
-        </el-menu-item-group>
-      </el-submenu>
-      <el-submenu index="/6">
-        <template slot="title"><i class="el-icon-postcard navigation"></i><span slot="title">编辑器</span>
-        </template>
-        <el-menu-item-group>
-          <el-menu-item index="/jsonEditor">jsonEditor</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
+      <template v-for="(route,index) in sidebarRoutes">
+        <el-submenu :index="index+''">
+          <template slot="title">
+            <i class="navigation" :class="'el-icon-'+route.meta.icon"></i>
+            <!--<i class="el-icon-message navigation"></i>-->
+            <span slot="title">{{route.name}}</span>
+          </template>
+          <el-menu-item-group>
+            <template v-for = "child in route.children">
+              <el-menu-item :index="child.path">{{child.name}}</el-menu-item>
+            </template>
+
+          </el-menu-item-group>
+        </el-submenu>
+      </template>
+
+      <!--<el-submenu index="/2">-->
+        <!--<template slot="title"><i class="el-icon-eleme navigation"></i><span slot="title">ELEMENTUI</span>-->
+        <!--</template>-->
+        <!--<el-menu-item-group>-->
+          <!--<el-menu-item index="/table">table</el-menu-item>-->
+          <!--<el-menu-item index="/form">form</el-menu-item>-->
+          <!--<el-menu-item index="/transfer">transfer</el-menu-item>-->
+          <!--<el-menu-item index="/checkboxDemo">checkboxDemo</el-menu-item>-->
+          <!--<el-menu-item index="/validtorForm">validtorForm</el-menu-item>-->
+          <!--<el-menu-item index="/mock">mock</el-menu-item>-->
+          <!--<el-menu-item index="/caneditortable">caneditortable</el-menu-item>-->
+          <!--<el-menu-item index="/eleeditortable">eleeditortable</el-menu-item>-->
+          <!--<el-menu-item index="/fatherdrag">fatherdrag</el-menu-item>-->
+          <!--<el-menu-item index="/mergeTable">mergeTable</el-menu-item>-->
+          <!--<el-menu-item index="/betterTables">betterTables</el-menu-item>-->
+          <!--<el-menu-item index="/two-lists">two-lists</el-menu-item>-->
+        <!--</el-menu-item-group>-->
+      <!--</el-submenu>-->
+      <!--<el-submenu index="/3">-->
+        <!--<template slot="title"><i class="el-icon-menu navigation"></i><span slot="title">第三方类库</span>-->
+        <!--</template>-->
+        <!--<el-menu-item-group>-->
+          <!--<el-menu-item index="/SKU">SKU</el-menu-item>-->
+          <!--<el-menu-item index="/handsontable">handsontable</el-menu-item>-->
+          <!--<el-menu-item index="/image-lazy">image-lazy</el-menu-item>-->
+          <!--<el-menu-item index="/html2canvas">html2canvas</el-menu-item>-->
+          <!--<el-menu-item index="/enlarge">enlarge</el-menu-item>-->
+          <!--<el-menu-item index="/better-scroll">better-scroll</el-menu-item>-->
+          <!--<el-menu-item index="/echarts">echarts</el-menu-item>-->
+          <!--<el-menu-item index="/waterfall">waterfall</el-menu-item>-->
+          <!--<el-menu-item index="/qrcode">二维码</el-menu-item>-->
+          <!--<el-menu-item index="/imageEditor">imageEditor</el-menu-item>-->
+          <!--<el-menu-item index="/imagecropper">imagecropper</el-menu-item>-->
+          <!--<el-menu-item index="/vue-scrollto">vue-scrollto</el-menu-item>-->
+          <!--<el-menu-item index="/vue-fabric">vue-fabric</el-menu-item>-->
+          <!--<el-menu-item index="/mergeImages">mergeImages</el-menu-item>-->
+          <!--<el-menu-item index="/baidu-map">baidu-map</el-menu-item>-->
+          <!--<el-menu-item index="/animate">animate</el-menu-item>-->
+        <!--</el-menu-item-group>-->
+      <!--</el-submenu>-->
+      <!--<el-submenu index="/4">-->
+        <!--<template slot="title"><i class="el-icon-document navigation"></i><span slot="title">NetWork</span>-->
+        <!--</template>-->
+        <!--<el-menu-item-group>-->
+          <!--<el-menu-item index="/loading">loading</el-menu-item>-->
+          <!--<el-menu-item index="/scrollTo">scrollTo</el-menu-item>-->
+          <!--<el-menu-item index="/xmForm">xmForm(N)</el-menu-item>-->
+          <!--<el-menu-item index="/TESTS">vuedraggable</el-menu-item>-->
+        <!--</el-menu-item-group>-->
+      <!--</el-submenu>-->
+      <!--<el-submenu index="/5">-->
+        <!--<template slot="title"><i class="el-icon-postcard navigation"></i><span slot="title">Example</span>-->
+        <!--</template>-->
+        <!--<el-menu-item-group>-->
+          <!--<el-menu-item index="/menu">menu</el-menu-item>-->
+          <!--&lt;!&ndash;<el-menu-item index="/vueScrollTo">menu</el-menu-item>&ndash;&gt;-->
+          <!--<el-submenu index="/">-->
+            <!--<template slot="title">兄弟组件传值</template>-->
+            <!--<el-menu-item index="/brotherone">brotherone</el-menu-item>-->
+            <!--<el-menu-item index="/brothertwo">brothertwo</el-menu-item>-->
+          <!--</el-submenu>-->
+          <!--<el-submenu index="/zjz">-->
+            <!--<template slot="title">jsx-render</template>-->
+            <!--<el-menu-item index="/render">render</el-menu-item>-->
+            <!--<el-menu-item index="/renderindex">renderindex</el-menu-item>-->
+          <!--</el-submenu>-->
+        <!--</el-menu-item-group>-->
+      <!--</el-submenu>-->
+      <!--<el-submenu index="/6">-->
+        <!--<template slot="title"><i class="el-icon-postcard navigation"></i><span slot="title">编辑器</span>-->
+        <!--</template>-->
+        <!--<el-menu-item-group>-->
+          <!--<el-menu-item index="/jsonEditor">jsonEditor</el-menu-item>-->
+        <!--</el-menu-item-group>-->
+      <!--</el-submenu>-->
     </el-menu>
   </el-scrollbar>
 </template>
 
 <script>
+  import { sidebarRoutes } from '../../router/index'
   export default {
     name: 'Sidebar',
     props:{
@@ -108,15 +109,24 @@
     },
     data () {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        msg: 'Welcome to Your Vue.js App',
+        sidebarRoutes:[]
       }
     },
     mounted(){
+      console.log(sidebarRoutes);
+      this.sidebarRoutes = sidebarRoutes
       console.log(this.activeNav);
     }
   }
 </script>
 
 <style scoped>
-
+  .navigation{
+    vertical-align: middle;
+    margin-right: 5px;
+    width: 24px;
+    text-align: center;
+    font-size: 18px;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,328 @@ import echartsMap from '@/components/echartsMap'
 
 Vue.use(Router)
 
+
+
+export const sidebarRoutes = [
+  {
+    path: '/Vue',
+    name: 'Vue',
+    meta :{
+      title: 'Vue',
+      icon: 'message',
+    },
+    redirect: '/helloworld',
+    component: () => import("../pagers/layout/index"),
+    children: [
+      {
+        path: '/testv-model',
+        name: 'testv-model',
+        component: () => import("../pagers/testv-model"),
+      },
+      {
+        path: '/testcascader',
+        name: 'testcascader',
+        component: () => import("../pagers/testcascader"),
+      },
+      {
+        path: '/mixins',
+        name: 'mixins',
+        component: () => import("../pagers/mixins"),
+      },
+      {
+        path: '/sync',
+        name: 'sync',
+        component: () => import("../pagers/sync"),
+      },
+      {
+        path: '/transitions',
+        name: 'transitions',
+        component: () => import("../pagers/transitiontest/demo"),
+      },
+      {
+        path: '/directive',
+        name: 'directive',
+        component: () => import("../pagers/directive"),
+      },
+      {
+        path: '/provideInject',
+        name: 'provideInject',
+        component: () => import("../pagers/vue-demo/provideInject/index"),
+      },
+      {
+        path: '/attrListener',
+        name: 'attrListener',
+        component: () => import("../pagers/vue-demo/attrListener/index"),
+      },
+      {
+        path: '/NativeDrag',
+        name: 'NativeDrag',
+        component: () => import("../pagers/own/NativeDrag"),
+      },
+      {
+        path: '/extendTable',
+        name: 'attrListener',
+        component: () => import("../pagers/vue-demo/table/extendTable"),
+      },
+    ]
+  },
+  {
+    path: '/ELEMENTUI',
+    name: 'ELEMENTUI',
+    meta :{
+      title: 'ELEMENTUI',
+      icon: 'eleme',
+    },
+    redirect: '/helloworld',
+    component: () => import("../pagers/layout/index"),
+    children: [
+      {
+        path: '/table',
+        name: 'table',
+        component: () => import("../pagers/BaseTable"),
+      },
+      {
+        path: '/form',
+        name: 'form',
+        component: () => import("../pagers/form"),
+      },
+      {
+        path: '/transfer',
+        name: 'transfer',
+        component: () => import("../pagers/element-demo/transferDemo"),
+      },
+      {
+        path: '/checkboxDemo',
+        name: 'checkboxDemo',
+        component: () => import("../pagers/element-demo/checkboxDemo"),
+      },
+      {
+        path: '/validtorForm',
+        name: 'validtorForm',
+        component: () => import("../pagers/validatorForm"),
+      },
+      {
+        path: '/mock',
+        name: 'mock',
+        component: () => import("../pagers/testmock"),
+      },
+      {
+        path: '/caneditortable',
+        name: 'caneditortable',
+        component: () => import("../pagers/caneditortable"),
+      },
+      {
+        path: '/eleeditortable',
+        name: 'eleeditortable',
+        component: () => import("../pagers/eleeditortable"),
+      },
+      {
+        path: '/fatherdrag',
+        name: 'fatherdrag',
+        component: () => import("../pagers/fatherdrag"),
+      },
+      {
+        path: '/mergeTable',
+        name: 'mergeTable',
+        component: () => import("../pagers/mergeTable"),
+      },
+      {
+        path: '/betterTables',
+        name: 'betterTables',
+        component: () => import("../pagers/betterTables"),
+      },
+      {
+        path: '/two-lists',
+        name: 'two-lists',
+        component: () => import("../pagers/VueDraggable/two-lists"),
+      },
+    ]
+  },
+  {
+    path: '/thirdLibrary',
+    name: '第三方类库',
+    redirect: '/helloworld',
+    meta :{
+      title: 'ELEMENTUI',
+      icon: 'menu',
+    },
+    component: () => import("../pagers/layout/index"),
+    children: [
+      {
+        path: '/SKU',
+        name: 'SKU',
+        component: () => import("../pagers/SKU"),
+      },
+      {
+        path: '/handsontable',
+        name: 'handsontable',
+        component: () => import("../pagers/handsontable"),
+      },
+      {
+        path: '/image-lazy',
+        name: 'image-lazy',
+        component: () => import("../pagers/image-lazy"),
+      },
+      {
+        path: '/html2canvas',
+        name: 'html2canvas',
+        component: () => import("../pagers/html2canvas"),
+      },
+      {
+        path: '/enlarge',
+        name: 'enlarge',
+        component: () => import("../pagers/enlarge"),
+      },
+      {
+        path: '/better-scroll',
+        name: 'better-scroll',
+        component: () => import("../pagers/better-scroll"),
+      },
+      {
+        path: '/echarts',
+        name: 'echarts',
+        component: () => import("../pagers/echarts"),
+      },
+      {
+        path: '/waterfall',
+        name: 'waterfall',
+        component: () => import("../pagers/waterfall"),
+      },
+      {
+        path: '/qrcode',
+        name: 'qrcode',
+        component: () => import("../pagers/qrcode/index"),
+      },
+      {
+        path: '/imageEditor',
+        name: 'imageEditor',
+        component: () => import("../pagers/imageEditor"),
+      },
+      {
+        path: '/imagecropper',
+        name: 'imagecropper',
+        component: () => import("../pagers/imagecropper"),
+      },
+      {
+        path: '/vue-scrollto',
+        name: 'vue-scrollto',
+        component: () => import("../pagers/third-part/scroll"),
+      },
+      {
+        path: '/vue-fabric',
+        name: 'vue-fabric',
+        component: () => import("../pagers/third-part/vue-fabric"),
+      },
+      {
+        path: '/mergeImages',
+        name: 'mergeImages',
+        component: () => import("../pagers/third-part/mergeImages"),
+      },
+      {
+        path: '/baidu-map',
+        name: 'baidu-map',
+        component: () => import("../pagers/map/index"),
+      },
+      {
+        path: '/animate',
+        name: 'animate',
+        component: () => import("../pagers/animate/index"),
+      },
+    ]
+  },
+  {
+    path: '/NetWork',
+    name: 'NetWork',
+    redirect: '/helloworld',
+    meta:{
+      title: 'NetWork',
+      icon: 'document',
+    },
+    component: () => import("../pagers/layout/index"),
+    children: [
+      {
+        path: '/loading',
+        name: 'loading',
+        component: () => import("../pagers/loading"),
+      },
+      {
+        path: '/scrollTo',
+        name: 'scrollTo',
+        component: () => import("../pagers/scrollTo"),
+      },
+      {
+        path: '/xmForm',
+        name: 'xmForm',
+        component: () => import("../pagers/form/xmForm"),
+      },
+      {
+        path: '/TESTS',
+        name: 'TESTS',
+        component: () => import("../pagers/TESTS"),
+      },
+    ]
+  },
+  {
+    path: '/Example',
+    name: 'Example',
+    meta:{
+      title: 'Example',
+      icon: 'postcard',
+    },
+    redirect: '/helloworld',
+    component: () => import("../pagers/layout/index"),
+    children: [
+      {
+        path: '/menu',
+        name: 'menu',
+        component: () => import("../pagers/menu"),
+      },
+      {
+        path: '/brotherone',
+        name: 'brotherone',
+        component: () => import("../pagers/brotherone"),
+      },
+      {
+        path: '/brothertwo',
+        name: 'brothertwo',
+        component: () => import("../pagers/brothertwo"),
+      },
+      {
+        path: '/render',
+        name: 'render',
+        component: () => import("../pagers/field/index"),
+      },
+      {
+        path: '/renderindex',
+        name: 'renderindex',
+        component: () => import("../pagers/field/renderindex"),
+      },
+    ]
+  },
+  {
+    path: '/Editor',
+    name: '编辑器',
+    meta:{
+      title: 'Example',
+      icon: 'postcard',
+    },
+    redirect: '/helloworld',
+    component: () => import("../pagers/layout/index"),
+    children: [
+      {
+        path: '/jsonEditor',
+        name: 'jsonEditor',
+        component: () => import("../pagers/jsonEditor/index"),
+      },
+      {
+        path: '/jsonEditor1',
+        name: 'jsonEditor1',
+        component: () => import("../pagers/jsonEditor/index"),
+      },
+    ]
+  },
+]
+
 export default new Router({
   routes: [
     {
@@ -41,248 +363,12 @@ export default new Router({
           component: echartsMap
         },
         {
-          path: '/testv-model',
-          name: 'testv-model',
-          component: () => import("../pagers/testv-model"),
-        },{
-          path: '/testcascader',
-          name: 'testcascader',
-          component: () => import("../pagers/testcascader"),
-        },
-        {
-          path: '/echarts',
-          name: 'echarts',
-          component: () => import("../pagers/echarts"),
-        },
-        {
-          path: '/waterfall',
-          name: 'waterfall',
-          component: () => import("../pagers/waterfall"),
-        },
-        {
-          path: '/table',
-          name: 'table',
-          component: () => import("../pagers/BaseTable"),
-        },
-        {
-          path: '/betterTables',
-          name: 'betterTables',
-          component: () => import("../pagers/betterTables"),
-        },
-        {
-          path: '/mergeTable',
-          name: 'mergeTable',
-          component: () => import("../pagers/mergeTable"),
-        },
-        {
-          path: '/SKU',
-          name: 'SKU',
-          component: () => import("../pagers/SKU"),
-        },
-        {
-          path: '/form',
-          name: 'form',
-          component: () => import("../pagers/form"),
-        },
-        {
-          path: '/imageEditor',
-          name: 'imageEditor',
-          component: () => import("../pagers/imageEditor"),
-        },
-        {
-          path: '/caneditortable',
-          name: 'caneditortable',
-          component: () => import("../pagers/caneditortable"),
-        },
-        {
-          path: '/better-scroll',
-          name: 'better-scroll',
-          component: () => import("../pagers/better-scroll"),
-        },
-        {
-          path: '/eleeditortable',
-          name: 'eleeditortable',
-          component: () => import("../pagers/eleeditortable"),
-        },
-        {
-          path: '/imagecropper',
-          name: 'imagecropper',
-          component: () => import("../pagers/imagecropper"),
-        },
-        {
-          path: '/menu',
-          name: 'menu',
-          component: () => import("../pagers/menu"),
-        },
-        {
-          path: '/enlarge',
-          name: 'enlarge',
-          component: () => import("../pagers/enlarge"),
-        },
-        {
-          path: '/mock',
-          name: 'mock',
-          component: () => import("../pagers/testmock"),
-        },
-        {
-          path: '/directive',
-          name: 'directive',
-          component: () => import("../pagers/directive"),
-        },
-        {
-          path: '/handsontable',
-          name: 'handsontable',
-          component: () => import("../pagers/handsontable"),
-        },
-        {
-          path: '/fatherdrag',
-          name: 'fatherdrag',
-          component: () => import("../pagers/fatherdrag"),
-        },
-        {
-          path: '/scrollTo',
-          name: 'scrollTo',
-          component: () => import("../pagers/scrollTo"),
-        },
-        {
-          path: '/brotherone',
-          name: 'brotherone',
-          component: () => import("../pagers/brotherone"),
-        },
-        {
-          path: '/brothertwo',
-          name: 'brothertwo',
-          component: () => import("../pagers/brothertwo"),
-        },
-        {
-          path: '/image-lazy',
-          name: 'image-lazy',
-          component: () => import("../pagers/image-lazy"),
-        },
-        {
-          path: '/mixins',
-          name: 'mixins',
-          component: () => import("../pagers/mixins"),
-        },
-        {
-          path: '/animate',
-          name: 'animate',
-          component: () => import("../pagers/animate/index"),
-        },
-        {
-          path: '/html2canvas',
-          name: 'html2canvas',
-          component: () => import("../pagers/html2canvas"),
-        },
-        {
-          path: '/loading',
-          name: 'loading',
-          component: () => import("../pagers/loading"),
-        },{
-          path: '/validtorForm',
-          name: 'validtorForm',
-          component: () => import("../pagers/validatorForm"),
-        },
-        {
-          path: '/xmForm',
-          name: 'xmForm',
-          component: () => import("../pagers/form/xmForm"),
-        },
-        {
-          path: '/sync',
-          name: 'sync',
-          component: () => import("../pagers/sync"),
-        },{
-          path: '/transitions',
-          name: 'transitions',
-          component: () => import("../pagers/transitiontest/demo"),
-        },
-        {
-          path: '/two-lists',
-          name: 'two-lists',
-          component: () => import("../pagers/VueDraggable/two-lists"),
-        },
-        {
-          path: '/qrcode',
-          name: 'qrcode',
-          component: () => import("../pagers/qrcode/index"),
-        },
-        {
-          path: '/provideInject',
-          name: 'provideInject',
-          component: () => import("../pagers/vue-demo/provideInject/index"),
-        },
-        {
-          path: '/attrListener',
-          name: 'attrListener',
-          component: () => import("../pagers/vue-demo/attrListener/index"),
-        },
-        {
-          path: '/extendTable',
-          name: 'attrListener',
-          component: () => import("../pagers/vue-demo/table/extendTable"),
-        },
-        {
-          path: '/render',
-          name: 'render',
-          component: () => import("../pagers/field/index"),
-        },
-        {
-          path: '/renderindex',
-          name: 'renderindex',
-          component: () => import("../pagers/field/renderindex"),
-        },
-        {
-          path: '/jsonEditor',
-          name: 'jsonEditor',
-          component: () => import("../pagers/jsonEditor/index"),
-        },
-        {
-          path: '/transfer',
-          name: 'transfer',
-          component: () => import("../pagers/element-demo/transferDemo"),
-        },
-        {
-          path: '/TESTS',
-          name: 'TESTS',
-          component: () => import("../pagers/TESTS"),
-        },
-        {
-          path: '/vue-scrollto',
-          name: 'vue-scrollto',
-          component: () => import("../pagers/third-part/scroll"),
-        },
-        {
-          path: '/vue-fabric',
-          name: 'vue-fabric',
-          component: () => import("../pagers/third-part/vue-fabric"),
-        },
-        {
-          path: '/mergeImages',
-          name: 'mergeImages',
-          component: () => import("../pagers/third-part/mergeImages"),
-        },
-        {
-          path: '/baidu-map',
-          name: 'baidu-map',
-          component: () => import("../pagers/map/index"),
-        },
-        {
-          path: '/NativeDrag',
-          name: 'NativeDrag',
-          component: () => import("../pagers/own/NativeDrag"),
-        },
-        {
-          path: '/checkboxDemo',
-          name: 'checkboxDemo',
-          component: () => import("../pagers/element-demo/checkboxDemo"),
-        },
-        {
           path: '*',
           name: '404',
           component: () => import("../pagers/404"),
         },
       ]
-    }
+    },
+    ...sidebarRoutes
   ]
 })
