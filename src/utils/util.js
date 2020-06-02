9b7e88448f6c892434ef4dc6c08b250bbd4fdb61
unknown <zhangjianzhong@infimind.com>
2020-06-02 17:03:31 +0800

    feat(utils.js): 新增构造树状结构的数组的工具方法

--- src/utils/util.js
+++ src/utils/util.js
@@ -41,3 +41,58 @@ export function stringify(obj) {
     }
     return arr.join('&')
 }
+// let authList = [
+//     {
+//       pid:-1,
+//       id:1,
+//       name: '商品管理'
+//     },
+//     {
+//       pid:1,
+//       id:2,
+//       name: '创建商品'
+//     },
+//     {
+//       pid:1,
+//       id:3,
+//       name: '商品列表'
+//     },
+//     {
+//       pid:-1,
+//       id:4,
+//       name: '首页'
+//     },
+//     {
+//       pid:-1,
+//       id:5,
+//       name: '发布管理'
+//     },
+//     {
+//       pid:5,
+//       id:6,
+//       name: '平台上货'
+//     },
+//     {
+//       pid:5,
+//       id:7,
+//       name: '发布列表'
+//     }
+//   ]
+/**
+ * 把一个有父子关系的一个数组变为类似于树状结构的数组
+ * @param {*} authList 一个数组
+ */
+export function getTreeList(arr) {
+    const menu = []
+    const menuMap = {}
+    arr.forEach(m => {
+        m.children = []
+        menuMap[m.id] = m
+        if (m.pid === -1) {
+            menu.push(m)
+        } else {
+            menuMap[m.pid] && menuMap[m.pid].children.push(m)
+        }
+    })
+    return menu
+}
